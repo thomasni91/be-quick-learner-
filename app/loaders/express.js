7c2b415083a21bef7f908b8563ab0f70c110f063
thomas ni <857840959@QQ.COM>
2022-11-13 20:40:15 +1100

    ss

--- app/loaders/express.js
+++ app/loaders/express.js
@@ -14,7 +14,7 @@ export default (app) => {
 	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc));
 	app.use(config.api.prefix, apiRouter);
 	app.get('/', (req, res) => {
-		res.json({ hello: 'World.a' });
+		res.json({ hello: 'World.b' });
 		logger.info('Server Sent A Hello World!');
 	});
 	return app;
